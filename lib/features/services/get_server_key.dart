a84c467b1e3c4b5830979c06eaf0a393b478a705
shubhamchavan123 <chavanshubham936@gmail.com>
2025-07-01 16:28:33 +0530

    working all app code play store 3

--- lib/features/services/get_server_key.dart
+++ lib/features/services/get_server_key.dart
@@ -3,9 +3,8 @@ import 'package:googleapis_auth/auth_io.dart';
 class GetServerKey {
   Future<String> getServerKeyToken() async {
     final scopes = [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/firebase.database',
-      'https://www.googleapis.com/auth/firebase.messaging',
+      '',
+
     ];
 
     final client = await clientViaServiceAccount(
